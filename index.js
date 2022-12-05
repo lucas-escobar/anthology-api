aa6bda504c586257e3c934c77fa3409067de1181
Lucas Escobar <lucas.an.escobar@gmail.com>
2022-12-05 13:26:12 -0500

    Remove body-parser middleware dependency
    Express added body-parser back into packaged middleware in 4.16.0

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
 import express from 'express';
 import cors from 'cors';
-import bodyParser from 'body-parser';
 
 const app = express();
 const port = process.env.PORT_NUMBER || 8080;
 
 app.use(cors());
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended:false}));
+app.use(express.json());
+app.use(express.urlencoded({extended:false}));
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
