326320ea8de8d9d14083645865980527a56c6d65
ESCOBAR <13lae@caslab.queensu.ca>
2022-12-05 02:43:38 -0500

    Create Database Script
    Add all entities from brainstorm diagram. Needs testing.

---
+++ lit_db.sql
@@ -0,0 +1,47 @@
+CREATE EXTENSION pgcrypto;
+
+CREATE TABLE group(
+    group_id        serial PRIMARY KEY,
+    name            text
+);
+
+CREATE TABLE user_groups(
+    fk_userGroups_group integer references group(group_id),
+    fk_userGroups_user  integer references user(user_id),
+    PRIMARY KEY(fk_userGroups_group, fk_userGroups_user)
+);
+
+CREATE TABLE user(
+    user_id             serial PRIMARY KEY,
+    username            text UNIQUE NOT NULL,
+    password            text NOT NULL,
+    email               text UNIQUE NOT NULL,
+);
+
+CREATE TABLE library(
+    library_id          serial PRIMARY KEY,
+    library_name        serial NOT NULL,
+    fk_library_group    integer references group(group_id)
+);
+
+CREATE TABLE book(
+    book_id         serial PRIMARY KEY,
+    title           text,
+    author          text[],
+    tags            text[],
+    publish_date    date,
+    cover_thumbnail bytea,
+    cover_url       text,
+    book_file_url   text UNIQUE,
+    fk_book_library integer references library(library_id)
+);
+
+CREATE TABLE excerpt(
+    excerpt_id          serial PRIMARY KEY,
+    content             text UNIQUE,
+    tags                text[],
+    bookmark            text,
+    fk_excerpt_user     integer references user(user_id),
+    fk_excerpt_library  integer references library(library_id),
+    fk_excerpt_book     integer references book(book_id)
+);
