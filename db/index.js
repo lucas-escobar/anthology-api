7737c8068ca57597ed638bc2e768eb659604d003
ESCOBAR <13lae@caslab.queensu.ca>
2022-12-07 17:45:19 -0500

    Load environment variables with dotenv

--- db/index.js
+++ db/index.js
@@ -1,12 +1,13 @@
-import { Pool } from 'pg';
+import pg from 'pg';
+import * as dotenv from 'dotenv';
+dotenv.config();
 
+export { query };
+
+const { Pool } = pg;
 const pool = new Pool();
 
 function query(text, param){
     if (param === undefined) { pool.query(text); }
     else { pool.query(text, param); }
-};
-
-module.exports = {
-    query
 };
