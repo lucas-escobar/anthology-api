b8e0dc81ee25daf0fa54a1073e9bc7e5687d2985
ESCOBAR <13lae@caslab.queensu.ca>
2022-12-07 17:41:33 -0500

    Rename group and user tables to remove keyword overlap

--- db/lit_db.sql
+++ db/lit_db.sql
@@ -1,36 +1,38 @@
+DROP SCHEMA public CASCADE;
+CREATE SCHEMA public;
 CREATE EXTENSION pgcrypto;
 
-CREATE TABLE group(
-    group_id        serial PRIMARY KEY,
-    name            text
+CREATE TABLE community(
+    community_id    serial PRIMARY KEY,
+    community_name  text
 );
 
-CREATE TABLE user_groups(
-    fk_userGroups_group integer references group(group_id),
-    fk_userGroups_user  integer references user(user_id),
-    PRIMARY KEY(fk_userGroups_group, fk_userGroups_user)
-);
-
-CREATE TABLE user(
-    user_id             serial PRIMARY KEY,
+CREATE TABLE app_user(
+    app_user_id         serial PRIMARY KEY,
     username            text UNIQUE NOT NULL,
     password            text NOT NULL,
-    email               text UNIQUE NOT NULL,
+    email               text UNIQUE NOT NULL
+);
+
+CREATE TABLE appUser_communities(
+    fk_appUserCommunities_community integer references community(community_id),
+    fk_appUserCommunities_appUser   integer references app_user(app_user_id),
+    PRIMARY KEY(fk_appUserCommunities_community, fk_appUserCommunities_appUser)
 );
 
 CREATE TABLE library(
-    library_id          serial PRIMARY KEY,
-    library_name        serial NOT NULL,
-    fk_library_group    integer references group(group_id)
+    library_id              serial PRIMARY KEY,
+    library_name            serial NOT NULL,
+    fk_library_community    integer references community(community_id)
 );
 
 CREATE TABLE book(
     book_id         serial PRIMARY KEY,
     title           text,
     author          text[],
-    UNIQUE(title, author),
+    UNIQUE(title, author),  -- TODO title equivalency test
     tags            text[],
-    publish_date    date,
+    publish_date    date,   -- TODO handle multiple publication dates
     cover_thumbnail bytea UNIQUE,
     cover_url       text UNIQUE,
     book_file_url   text UNIQUE,
@@ -42,7 +44,7 @@ CREATE TABLE excerpt(
     content             text UNIQUE,
     tags                text[],
     bookmark            text,
-    fk_excerpt_user     integer references user(user_id),
+    fk_excerpt_appUser  integer references app_user(app_user_id),
     fk_excerpt_library  integer references library(library_id),
     fk_excerpt_book     integer references book(book_id)
 );
