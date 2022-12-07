3ce8b4efddb2ef826b06eec1fbde434ad7f2348b
ESCOBAR <13lae@caslab.queensu.ca>
2022-12-07 17:33:26 -0500

    Refactor using imported functions

--- index.js
+++ index.js
@@ -1,19 +1,9 @@
 import express from 'express';
-import cors from 'cors';
-import Router from 'express-promise-router';
-import './db/index.js';
+import { mountRoutes } from './routes/index.js';
+import { mountSetup, connectPort } from './util/index.js';
 
 const app = express();
-const port = process.env.PORT_NUMBER || 8080;
 
-app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({extended:false}));
-
-app.get('/user', (req, res) => {
-    res.send("users");
-});
-
-app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
-});
+mountSetup(app);
+mountRoutes(app);
+connectPort(app);
