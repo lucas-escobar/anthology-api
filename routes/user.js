24a4fef99392c5fc1f83b7e99f3c298008bc4022
lucas-escobar <lucas.an.escobar@gmail.com>
2022-12-08 02:35:29 -0500

    Query database for list of communities related to user id

--- routes/user.js
+++ routes/user.js
@@ -5,11 +5,19 @@ export { userRouter };
 
 const userRouter = new Router();
 
-userRouter.get('/user/:input', (req, res) => {
-    const { input } = req.params;
-    const text = 'INSERT INTO community (community_name) VALUES ($1)';
-    query(text, [input]);
-    res.send("Input added to db")
+userRouter.get('/user/:id', async (req, res) => {
+    const user_communities_query = {
+        text:' \
+            SELECT community_name \
+            FROM app_user \
+            JOIN appUser_communities \
+                ON $1 = fk_appUserCommunities_appUser \
+            JOIN community \
+                ON fk_appUserCommunities_community = community_id',
+        values: [req.params.id]
+    }
+    const { rows } = await query(user_communities_query);
+    res.send(rows);
 });
 
 
